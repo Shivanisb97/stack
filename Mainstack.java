58569d821f8d25b41e4f8f5433b7ff9ce1ef80cc
shivani <shivanisb234@gmail.com>
2022-11-17 12:02:41 +0530

    new file

---
+++ Mainstack.java
@@ -0,0 +1,20 @@
+package stackArray;
+
+public class Mainstack {
+
+	public static void main(String[] args) {
+		Stack1 stack=new Stack1();
+		System.out.println(stack.isEmpty());
+		stack.push(5);
+		stack.push(10);
+		stack.push(7);
+		stack.push(2);
+		System.out.println(stack.isEmpty());
+		System.out.println(stack.peek());
+		System.out.println(stack.pop());
+		System.out.println(stack.peek());
+		//System.out.println(stack.pop());
+		//System.out.println(stack.pop());
+	}
+
+}
